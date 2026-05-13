4b7b86f274d7de8758fe664823fdce88f688532e
louckyshallis <shallis.loucks@drake.edu>
2026-05-12 20:50:35 -0500

    Testing new Data

--- flaskapp.py
+++ flaskapp.py
@@ -58,7 +58,7 @@ def display_users():
     # note that this could have been a result from an SQL query :) 
     users_list = (('John','Doe','Comedy'),('Jane', 'Doe','Drama'))
     query = "SELECT * LIMIT 10 FROM ProjectOneStore"
-    users_list2 = execute_query()
+    users_list2 = execute_query(query)
     return render_template('display_users.html', users = users_list2)
 
 
